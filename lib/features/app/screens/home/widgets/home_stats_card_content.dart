000599014ebe235b2b78d98dc84c0050bb5f166d
Logan Bradley <loganbradley787@gmail.com>
2024-08-28 09:55:38 -0400

    added FittedBoxes to StatsCardContent to avoid overflow

--- lib/features/app/screens/home/widgets/home_stats_card_content.dart
+++ lib/features/app/screens/home/widgets/home_stats_card_content.dart
@@ -20,38 +20,48 @@ class GenesisStatsCardContent extends StatelessWidget {
       children: [
         Expanded(
           flex: 8,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              FittedBox(child: Text(title, style: Theme.of(context).textTheme.titleMedium!)),
-              const Spacer(),
-              FittedBox(
-                child: RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: stat,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .apply(color: GenesisColors.grey)),
-                      TextSpan(
-                          text: units,
-                          style: Theme.of(context).textTheme.labelSmall!),
-                    ],
+          child: FittedBox(
+            fit: BoxFit.contain,
+            alignment: Alignment.centerLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FittedBox(
+                    child: Text(title,
+                        style: Theme.of(context).textTheme.titleMedium!)),
+                // const Spacer(),
+                FittedBox(
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: stat,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .apply(color: GenesisColors.grey)),
+                        TextSpan(
+                            text: units,
+                            style: Theme.of(context).textTheme.labelSmall!),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
         Expanded(
           flex: 4,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              icon,
-            ],
+          child: FittedBox(
+            fit: BoxFit.contain,
+            alignment: Alignment.centerRight,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                icon,
+              ],
+            ),
           ),
         ),
       ],
