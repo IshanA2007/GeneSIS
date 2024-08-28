0d266f22035832d968924cfddd54d75901212b70
Logan Bradley <loganbradley787@gmail.com>
2024-08-28 09:44:13 -0400

    fixed overflow issue on gradebook (probably)

--- lib/features/app/screens/gradebook/widgets/gradebook_gradecard.dart
+++ lib/features/app/screens/gradebook/widgets/gradebook_gradecard.dart
@@ -28,83 +28,103 @@ class GradeCard extends StatelessWidget {
         // left - class name, change %, missing assignments
         Expanded(
           flex: 8,
-          child: FittedBox(fit: BoxFit.fitHeight, alignment: Alignment.centerLeft, child: Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                className,
-                style: Theme.of(context).textTheme.headlineSmall!.apply(
-                    color: dark ? GenesisColors.white : GenesisColors.white),
+              Expanded(
+                flex: 1,
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    className,
+                    style: Theme.of(context).textTheme.headlineSmall!.apply(
+                        color:
+                            dark ? GenesisColors.white : GenesisColors.white),
+                  ),
+                ),
+                // const Spacer(),
               ),
-              // const Spacer(),
-              RichText(
-                text: TextSpan(
+              Expanded(
+                flex: 1,
+                child: Row(
                   children: [
-                    TextSpan(
-                      text:
-                          "${monthlyChange >= 0 ? '+' : '-'}${monthlyChange.abs()}%",
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        color: monthlyChange >= 0
-                            ? Colors.greenAccent
-                            : Colors.redAccent,
-                      ),
-                    ),
-                    const TextSpan(
-                      text: " change this month",
-                      style: TextStyle(
-                        fontSize: 10.0,
-                        color: Colors.grey,
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text:
+                                    "${monthlyChange >= 0 ? '+' : '-'}${monthlyChange.abs()}%",
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: monthlyChange >= 0
+                                      ? Colors.greenAccent
+                                      : Colors.redAccent,
+                                ),
+                              ),
+                              const TextSpan(
+                                text: " change this month",
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // const Spacer(),
+                        Text(
+                          '$missingAssignments missing assignments',
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        // const Spacer(),
+                      ],
                     ),
                   ],
-                ),
-              ),
-              // const Spacer(),
-              Text(
-                '$missingAssignments missing assignments',
-                style: const TextStyle(
-                  fontSize: 10.0,
-                  color: Colors.grey,
-                ),
-              ),
-              // const Spacer(),
-              ],
-            ),
-          ),
-        ),
-        // right - grade letter, percent
-        Expanded(
-          flex: 4,
-          child: FittedBox(fit: BoxFit.contain, child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                letterGrade,
-                style: const TextStyle(
-                  fontSize: 60.0,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.tealAccent,
-                ),
-                textHeightBehavior: const TextHeightBehavior(
-                  applyHeightToFirstAscent: false,
-                  applyHeightToLastDescent: false,
-                ),
-              ),
-              Text(
-                '${gradePercent.toStringAsFixed(2)}%',
-                style: const TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.grey,
-                ),
-                textHeightBehavior: const TextHeightBehavior(
-                  applyHeightToFirstAscent: false,
-                  applyHeightToLastDescent: false,
                 ),
               ),
             ],
           ),
         ),
+        // right - grade letter, percent
+        Expanded(
+          flex: 4,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  letterGrade,
+                  style: const TextStyle(
+                    fontSize: 60.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.tealAccent,
+                  ),
+                  textHeightBehavior: const TextHeightBehavior(
+                    applyHeightToFirstAscent: false,
+                    applyHeightToLastDescent: false,
+                  ),
+                ),
+                Text(
+                  '${gradePercent.toStringAsFixed(2)}%',
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey,
+                  ),
+                  textHeightBehavior: const TextHeightBehavior(
+                    applyHeightToFirstAscent: false,
+                    applyHeightToLastDescent: false,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ],
     );
