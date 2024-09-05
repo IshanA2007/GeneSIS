4e18a583bed08edbc3c3276a33aa677861f344dc
Ishan Ajwani <ishanajwani@Ishans-Air.fios-router.home>
2024-09-05 17:06:41 -0400

    grades view ui complete (overflow fix pending)

---
+++ lib/features/app/screens/gradebook/widgets/gradebook_gradesview_infocard.dart
@@ -0,0 +1,194 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/genesis_card.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/sizes.dart';
+
+class GradesViewInfoCard extends StatelessWidget {
+  const GradesViewInfoCard({
+    super.key,
+    required this.weeklyChange,
+    required this.monthlyChange,
+    required this.semesterChange,
+  });
+
+  final int weeklyChange;
+  final int monthlyChange;
+  final int semesterChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: GenesisSizes.md),
+      child: GenesisCard(
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Grades",
+                        style: Theme.of(context).textTheme.titleLarge!),
+                    const SizedBox(height: GenesisSizes.sm),
+                    const Text(
+                      "A",
+                      style: TextStyle(
+                        fontSize: 60.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.tealAccent,
+                      ),
+                      textHeightBehavior: TextHeightBehavior(
+                        applyHeightToFirstAscent: false,
+                        applyHeightToLastDescent: false,
+                      ),
+                    ),
+                    Text(
+                      '${94.toStringAsFixed(2)}%',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleSmall!
+                          .apply(color: GenesisColors.grey),
+                      textHeightBehavior: const TextHeightBehavior(
+                        applyHeightToFirstAscent: false,
+                        applyHeightToLastDescent: false,
+                      ),
+                    ),
+                    const SizedBox(height: GenesisSizes.spaceBtwItems),
+                    Text(
+                      '155 missing assignments',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelMedium!
+                          .apply(color: GenesisColors.darkGrey),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Trends",
+                        style: Theme.of(context).textTheme.titleLarge!),
+                    const SizedBox(height: GenesisSizes.spaceBtwItems),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text:
+                            "${weeklyChange >= 0 ? '+' : '-'}${weeklyChange.abs()}%",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .apply(
+                              color: weeklyChange >= 0
+                                  ? Colors.greenAccent
+                                  : Colors.redAccent,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " change this week",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium!
+                                .apply(color: GenesisColors.darkerGrey),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: GenesisSizes.sm),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text:
+                            "${monthlyChange >= 0 ? '+' : '-'}${monthlyChange.abs()}%",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .apply(
+                              color: monthlyChange >= 0
+                                  ? Colors.greenAccent
+                                  : Colors.redAccent,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " change this month",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium!
+                                .apply(color: GenesisColors.darkerGrey),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: GenesisSizes.sm),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text:
+                            "${semesterChange >= 0 ? '+' : '-'}${semesterChange.abs()}%",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .apply(
+                              color: semesterChange >= 0
+                                  ? Colors.greenAccent
+                                  : Colors.redAccent,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " change this semester",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium!
+                                .apply(color: GenesisColors.darkerGrey),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // this one needs to be at the bottom
+                    const SizedBox(height: GenesisSizes.spaceBtwItems),
+
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Boosts your GPA ",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium!
+                                .apply(color: GenesisColors.darkerGrey),
+                          ),
+                          TextSpan(
+                            text:
+                            "${monthlyChange >= 0 ? '+' : '-'}${monthlyChange.abs()}%",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .apply(
+                              color: monthlyChange >= 0
+                                  ? Colors.greenAccent
+                                  : Colors.redAccent,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/app/screens/gradebook/widgets/gradebook_gradeview_assignment.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:grades/common/widgets/genesis_card.dart';
+import 'package:grades/features/app/screens/gradebook/widgets/gradebook_gradesview_infocard.dart';
+import 'package:grades/features/app/screens/gradebook/widgets/gradebook_gradesviewappbar.dart';
+import 'package:grades/utils/constants/colors.dart';
+import 'package:grades/utils/constants/sizes.dart';
+import 'package:grades/utils/device/device_utilities.dart';
+
+import 'gradebook_gradesview_gradebars.dart';
+
+class GradesViewAssignment extends StatelessWidget {
+  const GradesViewAssignment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: GenesisSizes.cardPaddingLg),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Text(
+              "Assignment",
+              textAlign: TextAlign.start,
+              style: Theme.of(context)
+                  .textTheme
+                  .labelMedium!
+                  .apply(color: GenesisColors.white),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Text("10/10",
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .apply(color: GenesisColors.white)),
+          ),
+          Expanded(
+            flex: 4,
+            child: Text("100%",
+                textAlign: TextAlign.end,
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .apply(color: GenesisColors.white)),
+          ),
+        ],
+      ),
+    );
+  }
+}
