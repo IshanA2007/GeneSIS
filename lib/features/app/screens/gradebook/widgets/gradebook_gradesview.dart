c25720599d82287128137c296a3d2e78234faba6
Ishan Ajwani <ishanajwani@Ishans-Air.fios-router.home>
2024-09-05 17:06:41 -0400

    grades view ui complete (overflow fix pending)

--- lib/features/app/screens/gradebook/widgets/gradebook_gradesview.dart
+++ lib/features/app/screens/gradebook/widgets/gradebook_gradesview.dart
@@ -1,56 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:grades/common/widgets/genesis_card.dart';
+import 'package:grades/features/app/screens/gradebook/widgets/gradebook_gradesview_infocard.dart';
 import 'package:grades/features/app/screens/gradebook/widgets/gradebook_gradesviewappbar.dart';
 import 'package:grades/utils/constants/colors.dart';
 import 'package:grades/utils/constants/sizes.dart';
 import 'package:grades/utils/device/device_utilities.dart';
 
 import 'gradebook_gradesview_gradebars.dart';
+import 'gradebook_gradeview_assignment.dart';
 
 class GradesView extends StatelessWidget {
   const GradesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    var weeklyChange = 0;
+    var monthlyChange = 4;
+    var semesterChange = -100;
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            GradesViewAppBar(
-              className: "Mobile/Web Research",
+            const GradesViewAppBar(
+              className: "Mobile WebAppRes TJ AV",
               gpaBoost: "1.0",
             ),
-            SizedBox(
+            const SizedBox(
               height: GenesisSizes.spaceBtwSections,
             ),
-            GradesViewGradeBars(),
+            const GradesViewGradeBars(),
+            GradesViewInfoCard(
+                weeklyChange: weeklyChange,
+                monthlyChange: monthlyChange,
+                semesterChange: semesterChange),
+            const SizedBox(height: GenesisSizes.lg),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: GenesisSizes.md),
+              padding: const EdgeInsets.symmetric(horizontal: GenesisSizes.md),
               child: GenesisCard(
                 child: SizedBox(
                   width: double.infinity,
-                  child: Row(
+                  child: Column(
                     children: [
-
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                      Text("Assignments",
+                          style: Theme.of(context).textTheme.titleLarge!),
+                      const SizedBox(height: GenesisSizes.sm),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: GenesisSizes.cardPaddingLg),
+                        child: Row(
                           children: [
-                            Text("Grades"),
+                            Expanded(
+                              flex: 4,
+                              child: Text(
+                                "NAME",
+                                textAlign: TextAlign.start,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelSmall!
+                                    .apply(color: GenesisColors.darkerGrey),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text("POINTS",
+                                  textAlign: TextAlign.center,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelSmall!
+                                      .apply(color: GenesisColors.darkerGrey)),
+                            ),
+                            Expanded(
+                              flex: 4,
+                              child: Text("GRADE",
+                                  textAlign: TextAlign.end,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelSmall!
+                                      .apply(color: GenesisColors.darkerGrey)),
+                            ),
                           ],
                         ),
                       ),
+                      const SizedBox(height: GenesisSizes.xs),
+                      for (int i = 0; i < 15; i++) ...[
+                        GradesViewAssignment(),
+                        const SizedBox(height: GenesisSizes.sm), // Add SizedBox after each item
+                      ],
 
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text("Trends"),
-                          ],
-                        ),
-                      ),
                     ],
                   ),
                 ),
--- lib/features/app/screens/gradebook/widgets/gradebook_gradesview_gradebars.dart
+++ lib/features/app/screens/gradebook/widgets/gradebook_gradesview_gradebars.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:grades/utils/constants/colors.dart';
 import 'package:grades/utils/device/device_utilities.dart';
@@ -35,46 +34,47 @@ class GradesViewGradeBars extends StatelessWidget {
                   children: [
                     // Background taller container
                     Container(
-                      height: (GenesisDeviceUtils.getScreenHeight() *
-                          0.3 *
-                          0.30), // Slightly taller
+                      height: GenesisDeviceUtils.getScreenHeight() * 0.3 * 0.30,
                       width: GenesisDeviceUtils.getScreenWidth() * 0.27,
                       decoration: BoxDecoration(
                         color: GenesisColors.gradeBarSecondary,
-                        // Change to differentiate
                         borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(30),
                           topRight: Radius.circular(30),
                         ),
                       ),
                     ),
-                    // Foreground main container
-                    Container(
-                      height: (GenesisDeviceUtils.getScreenHeight() *
-                          0.3 *
-                          0.30) *
-                          .67,
-                      width: GenesisDeviceUtils.getScreenWidth() * 0.27,
-                      decoration: BoxDecoration(
-                        color: GenesisColors.primaryColor,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(35),
-                          topRight: Radius.circular(35),
-                        ),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '68.33%',
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelSmall!
-                                .apply(color: GenesisColors.white),
+                    // Foreground main container with animation
+                    TweenAnimationBuilder<double>(
+                      tween: Tween(begin: 1.0, end: GenesisDeviceUtils.getScreenHeight() * 0.3 * 0.30 * 0.67),
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.easeOut,  // Smooth transition
+                      builder: (context, height, child) {
+                        return Container(
+                          height: height,
+                          width: GenesisDeviceUtils.getScreenWidth() * 0.27,
+                          decoration: BoxDecoration(
+                            color: GenesisColors.primaryColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(35),
+                              topRight: Radius.circular(35),
+                            ),
                           ),
-                          // Add more widgets as needed
-                        ],
-                      ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '68.33%',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelSmall!
+                                    .apply(color: GenesisColors.white),
+                              ),
+                              // Add more widgets as needed
+                            ],
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
@@ -98,46 +98,47 @@ class GradesViewGradeBars extends StatelessWidget {
                   children: [
                     // Background taller container
                     Container(
-                      height: (GenesisDeviceUtils.getScreenHeight() *
-                          0.3 *
-                          0.70), // Slightly taller
+                      height: GenesisDeviceUtils.getScreenHeight() * 0.3 * 0.70,
                       width: GenesisDeviceUtils.getScreenWidth() * 0.27,
                       decoration: BoxDecoration(
                         color: GenesisColors.gradeBarSecondary,
-                        // Change to differentiate
                         borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(30),
                           topRight: Radius.circular(30),
                         ),
                       ),
                     ),
-                    // Foreground main container
-                    Container(
-                      height: (GenesisDeviceUtils.getScreenHeight() *
-                          0.3 *
-                          0.70) *
-                          .67,
-                      width: GenesisDeviceUtils.getScreenWidth() * 0.27,
-                      decoration: BoxDecoration(
-                        color: GenesisColors.primaryColor,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(35),
-                          topRight: Radius.circular(35),
-                        ),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '62.40%',
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelSmall!
-                                .apply(color: GenesisColors.white),
+                    // Foreground main container with animation
+                    TweenAnimationBuilder<double>(
+                      tween: Tween(begin: 1.0, end: GenesisDeviceUtils.getScreenHeight() * 0.3 * 0.70 * 0.67),
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.easeOut,
+                      builder: (context, height, child) {
+                        return Container(
+                          height: height,
+                          width: GenesisDeviceUtils.getScreenWidth() * 0.27,
+                          decoration: BoxDecoration(
+                            color: GenesisColors.primaryColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(35),
+                              topRight: Radius.circular(35),
+                            ),
                           ),
-                          // Add more widgets as needed
-                        ],
-                      ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '62.40%',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelSmall!
+                                    .apply(color: GenesisColors.white),
+                              ),
+                              // Add more widgets as needed
+                            ],
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
