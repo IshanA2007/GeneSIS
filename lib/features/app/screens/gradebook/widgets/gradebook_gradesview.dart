c23b860cd79abf5caec364a7a6ad56666564781c
Ishan Ajwani <ishanajwani@Ishans-MacBook-Air.local>
2024-09-05 12:14:30 -0400

    i

--- lib/features/app/screens/gradebook/widgets/gradebook_gradesview.dart
+++ lib/features/app/screens/gradebook/widgets/gradebook_gradesview.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:grades/common/widgets/genesis_card.dart';
 import 'package:grades/features/app/screens/gradebook/widgets/gradebook_gradesviewappbar.dart';
+import 'package:grades/utils/constants/colors.dart';
+import 'package:grades/utils/constants/sizes.dart';
+import 'package:grades/utils/device/device_utilities.dart';
+
+import 'gradebook_gradesview_gradebars.dart';
 
 class GradesView extends StatelessWidget {
   const GradesView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
@@ -13,9 +20,42 @@ class GradesView extends StatelessWidget {
               className: "Mobile/Web Research",
               gpaBoost: "1.0",
             ),
-            // grades graph,
-            // grades/trends boxes,
-            // GradesViewAssignments(assignments: [{title: "assignment1", points: "30", outof: "30"}])
+            SizedBox(
+              height: GenesisSizes.spaceBtwSections,
+            ),
+            GradesViewGradeBars(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: GenesisSizes.md),
+              child: GenesisCard(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text("Grades"),
+                          ],
+                        ),
+                      ),
+
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text("Trends"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
