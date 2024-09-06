34bfaa01d7ae6f95670965571836e74cb8ddb16e
Ishan Ajwani <ishanajwani@Ishans-Air.fios-router.home>
2024-09-05 23:11:26 -0400

    firebase initialized, local storage started

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:grades/app.dart';
 import 'package:grades/utils/constants/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'data/repositories/authentication/authentication_repository.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  //Widget Binding
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  //Initialize Local Storage
+  await GetStorage.init();
+
+  //Native Splash Screen
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  //Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
+  //Load Application
   runApp(const MainApp());
 }
+
+
