148df6b161275bc636470e00470ac2235d833fdb
Ishan Ajwani <ishanajwani@Ishans-Air.fios-router.home>
2024-09-05 23:11:26 -0400

    firebase initialized, local storage started

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:grades/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:grades/utils/constants/colors.dart';
 import 'package:grades/utils/theme/theme.dart';
 
 class MainApp extends StatelessWidget {
@@ -16,7 +17,12 @@ class MainApp extends StatelessWidget {
           themeMode: ThemeMode.system,
           theme: GenesisTheme.lightTheme,
           darkTheme: GenesisTheme.darkTheme,
-          home: const OnBoardingScreen(),
+          home: const Scaffold(
+            backgroundColor: GenesisColors.primaryColor,
+            body: Center(
+              child: CircularProgressIndicator(color: Colors.white),
+            ),
+          ),
         );
       },
     );
