27d0d38dc79e30bb95c72ef4169110c5d11c9f45
Logan Bradley <loganbradley787@gmail.com>
2024-09-08 18:36:48 -0400

    emergency fix - null check on localstorage for logincontroller

--- lib/features/authentication/controllers/login/login_controller.dart
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -21,8 +21,8 @@ class LoginController extends GetxController {
 
   @override
   void onInit() {
-    email.text = localStorage.read("REMEMBER_ME_EMAIL");
-    password.text = localStorage.read("REMEMBER_ME_PASSWORD");
+    email.text = localStorage.read("REMEMBER_ME_EMAIL") ?? "";
+    password.text = localStorage.read("REMEMBER_ME_PASSWORD") ?? "";
   }
 
   Future<void> emailAndPasswordSignIn() async {
