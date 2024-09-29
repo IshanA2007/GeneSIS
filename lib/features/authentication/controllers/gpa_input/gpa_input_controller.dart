3b1345ef7510c53ff8ec865c08b569f3eb5bc13b
Ishan Ajwani <ishanajwani@Ishans-MacBook-Air.local>
2024-09-29 16:04:59 -0400

    gpa history

--- lib/features/authentication/controllers/gpa_input/gpa_input_controller.dart
+++ lib/features/authentication/controllers/gpa_input/gpa_input_controller.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:grades/common/data/ClassData.dart';
 import 'package:grades/common/data/GPAData.dart';
+import 'package:grades/common/data/Period.dart';
+import 'package:grades/common/data/User.dart';
 import 'package:grades/data/repositories/authentication/authentication_repository.dart';
 import 'package:grades/features/authentication/controllers/network/network_manager.dart';
 import 'package:grades/features/authentication/controllers/user/user_controller.dart';
 import 'package:grades/navigation_menu.dart';
 import 'package:grades/utils/constants/image_strings.dart';
 import 'package:grades/utils/helpers/grade_calculations.dart';
+import 'package:grades/utils/helpers/helper_functions.dart';
 import 'package:grades/utils/popups/full_screen_loader.dart';
 import 'package:grades/utils/validators/validation.dart';
 
@@ -37,11 +41,55 @@ class GPAInputController extends GetxController {
       user.curUser!.creditsTaken = double.parse(courseCreditsTaken.text.trim());
 
       double updatedCGPA = GenesisGradeCalculations.calculateCumulativeGPA(
-          user.curUser!, 1); //TODO: calculate current quarter
-      History overallHistory =
-      user.curUser!.history.firstWhere((history) => history.name == "overall");
-      overallHistory.history
-          .insert(0, GPAData(updatedCGPA));
+          user.curUser!,
+          GenesisHelpers.currentQuarter()); //TODO: calculate current quarter
+      History overallHistory = user.curUser!.history
+          .firstWhere((history) => history.name == "overall");
+      overallHistory.history.insert(0, GPAData(updatedCGPA));
+
+      User curUser = user.curUser!;
+      DateTime startDate = DateTime(2024, 8, 19);
+
+      // Get the current date
+      DateTime endDate = DateTime.now();
+
+      List<DateTime> dateList =
+          GenesisHelpers.generateDateList(startDate, endDate);
+
+      for (DateTime date in dateList) {
+        double outdatedGPA = curUser.initialCumGPA ?? 0.0;
+        double creditsTaken = curUser.creditsTaken ?? 1.0;
+        double gpaVal = outdatedGPA * creditsTaken;
+        if (GenesisHelpers.getQuarterOfDate(date) == 1 ||
+            GenesisHelpers.getQuarterOfDate(date) == 2) {
+          for (Period period in curUser.periods) {
+            ClassData curClass =
+                period.classData[GenesisHelpers.getQuarterOfDate(date) - 1];
+            if (curClass.gradebookCode == "UK" ||
+                curClass.gradebookCode == "rolling") {
+              gpaVal += GenesisGradeCalculations.gpaFromLetter(
+                      GenesisGradeCalculations.percentToLetter(
+                          GenesisGradeCalculations.calculateGradeOn(
+                              date: date, course: curClass)),
+                      curClass.courseName) *
+                  0.5;
+              creditsTaken += 0.5;
+            } else {
+              //TODO: store quarterly grades in ClassData somehow
+              //temp solution, just use current grade
+              gpaVal += GenesisGradeCalculations.gpaFromLetter(
+                      GenesisGradeCalculations.percentToLetter(
+                          curClass.percent),
+                      curClass.courseName) *
+                  0.5;
+              creditsTaken += 0.5;
+            }
+          }
+        }
+        overallHistory.history.add(GPAData(gpaVal / creditsTaken));
+
+        //TODO: implement q2-q4
+      }
 
       Map<dynamic, dynamic> users = localStorage.read("users");
       users[localStorage.read("username")] = user.curUser!.toMap();
--- lib/utils/helpers/helper_functions.dart
+++ lib/utils/helpers/helper_functions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class GenesisHelpers {
   static double getWidth() {
@@ -8,4 +9,30 @@ class GenesisHelpers {
   static bool isDarkMode(BuildContext context) {
     return Theme.of(context).brightness == Brightness.dark;
   }
+
+  static int currentQuarter() {
+    return 1;
+  }
+
+  static int getQuarterOfDate(DateTime date) {
+    return 1;
+  }
+
+  static List<DateTime> generateDateList(DateTime startDate, DateTime endDate) {
+    List<DateTime> dates = [];
+
+    // Add each date from startDate to endDate
+    for (DateTime date = startDate;
+        date.isBefore(endDate) || date.isAtSameMomentAs(endDate);
+        date = date.add(Duration(days: 1))) {
+      dates.add(date);
+    }
+
+    return dates;
+  }
+  static DateTime stringToDateTime(String dateString) {
+    DateFormat format = DateFormat("M/d/yyyy");
+    return format.parse(dateString);
+  }
+  
 }
