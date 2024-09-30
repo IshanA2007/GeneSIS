260d6c397b8f212d809074ddb6ea11dcbf24d901
Ishan Ajwani <ishanajwani@Ishans-Air.fios-router.home>
2024-09-30 16:34:18 -0400

    privacy policy

--- lib/features/authentication/screens/login/widgets/login_policy_view.dart
+++ lib/features/authentication/screens/login/widgets/login_policy_view.dart
@@ -1,50 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:grades/common/widgets/appbar/appbar.dart';
+import 'package:grades/utils/constants/sizes.dart';
+import 'package:grades/utils/constants/colors.dart';
+import 'package:grades/utils/helpers/helper_functions.dart';
 
 class PolicyView extends StatelessWidget {
   const PolicyView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = GenesisHelpers.isDarkMode(context);
     return Scaffold(
-      appBar: AppBar(title: const Text("Privacy Policy")),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+      appBar: GenesisAppBar(
+        title: Text(
+          "Privacy Policy",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        showBackArrow: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(GenesisSizes.defaultSpacing),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Privacy Policy",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              "1. Introduction",
+              style: Theme.of(context).textTheme.headlineSmall!.apply(
+                  color: dark ? GenesisColors.white : GenesisColors.black),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: GenesisSizes.spaceBtwItems),
             Text(
-              "Effective date: [Insert Date]",
-              style: TextStyle(fontSize: 16),
+              "We are committed to safeguarding the privacy of our users. This Privacy Policy outlines how we handle your personal data when using our application, particularly focusing on login credentials and academic information.",
+              style: Theme.of(context).textTheme.bodyMedium!.apply(
+                  color: dark ? GenesisColors.grey : GenesisColors.darkGrey),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: GenesisSizes.spaceBtwSections),
+
             Text(
-              "1. Introduction\n"
-              "We value your privacy and are committed to protecting your personal data. "
-              "This Privacy Policy explains how we collect, use, and share information about you when you use our application.",
-              style: TextStyle(fontSize: 16),
+              "2. Collection and Use of Personal Information",
+              style: Theme.of(context).textTheme.headlineSmall!.apply(
+                  color: dark ? GenesisColors.white : GenesisColors.black),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: GenesisSizes.spaceBtwItems),
             Text(
-              "2. Information We Collect\n"
-              "We may collect the following types of information:\n"
-              "- Personal Information: Such as your name, email address, and any other information you provide.\n"
-              "- Usage Data: Information about how you use our application, including your IP address, device type, and operating system.",
-              style: TextStyle(fontSize: 16),
+              "Our app collects only the data necessary for functionality, specifically your username and password for authenticating with the SIS StudentVue system. This information is solely used to retrieve your academic data and is never stored or transmitted to any third party.",
+              style: Theme.of(context).textTheme.bodyMedium!.apply(
+                  color: dark ? GenesisColors.grey : GenesisColors.darkGrey),
             ),
-            SizedBox(height: 16),
-            Text("3. How We Use Your Information\n"
-                "We may use your information for the following purposes:\n"
-                "- To provide and maintain our service.\n"
-                "- To notify you about changes to our service.\n"
-                "- To allow you to participate in interactive features of our service when you choose to do so.\n"
-                "- To provide customer support.\n"
-                "- To gather analysis or valuable information so that we can improve our service.\n"
-                "- To"),
+            const SizedBox(height: GenesisSizes.spaceBtwSections),
+
+            Text(
+              "3. Login Credentials and Security",
+              style: Theme.of(context).textTheme.headlineSmall!.apply(
+                  color: dark ? GenesisColors.white : GenesisColors.black),
+            ),
+            const SizedBox(height: GenesisSizes.spaceBtwItems),
+            Text(
+              "When you input your login details, they are immediately sent directly to the SIS StudentVue system for authentication. At no point are your username and password stored on external servers. If you opt to use the 'Remember Me' feature, your credentials are securely stored locally on your device only, and are never accessible by us or any third party.",
+              style: Theme.of(context).textTheme.bodyMedium!.apply(
+                  color: dark ? GenesisColors.grey : GenesisColors.darkGrey),
+            ),
+            const SizedBox(height: GenesisSizes.spaceBtwSections),
+
+            Text(
+              "4. Academic Data",
+              style: Theme.of(context).textTheme.headlineSmall!.apply(
+                  color: dark ? GenesisColors.white : GenesisColors.black),
+            ),
+            const SizedBox(height: GenesisSizes.spaceBtwItems),
+            Text(
+              "Once authenticated, your academic data is retrieved from SIS StudentVue and displayed within the app. This data is never stored externally; it is processed only for the purpose of being displayed on your device. No sensitive academic information is saved on external databases, ensuring that only you have access to your academic records.",
+              style: Theme.of(context).textTheme.bodyMedium!.apply(
+                  color: dark ? GenesisColors.grey : GenesisColors.darkGrey),
+            ),
+            const SizedBox(height: GenesisSizes.spaceBtwSections),
+
+            Text(
+              "5. Data Security",
+              style: Theme.of(context).textTheme.headlineSmall!.apply(
+                  color: dark ? GenesisColors.white : GenesisColors.black),
+            ),
+            const SizedBox(height: GenesisSizes.spaceBtwItems),
+            Text(
+              "We take security very seriously and employ industry-standard measures to protect your data. As your username and password are not stored externally, and all academic data is simply read and displayed on your device without being retained, we minimize any potential risk of data exposure.",
+              style: Theme.of(context).textTheme.bodyMedium!.apply(
+                  color: dark ? GenesisColors.grey : GenesisColors.darkGrey),
+            ),
+            const SizedBox(height: GenesisSizes.spaceBtwSections),
+
+            Text(
+              "6. Conclusion",
+              style: Theme.of(context).textTheme.headlineSmall!.apply(
+                  color: dark ? GenesisColors.white : GenesisColors.black),
+            ),
+            const SizedBox(height: GenesisSizes.spaceBtwItems),
+            Text(
+              "Your privacy is our utmost priority. By using this app, you can be assured that we do not store or share any sensitive information like your username, password, or academic data externally. We are committed to respecting your privacy and ensuring the security of your personal data at all times.",
+              style: Theme.of(context).textTheme.bodyMedium!.apply(
+                  color: dark ? GenesisColors.grey : GenesisColors.darkGrey),
+            ),
           ],
         ),
       ),
