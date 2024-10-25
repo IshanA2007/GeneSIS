9245202e7727beaad2d064a092bc67ac6ddf37e7
Logan Bradley <loganbradley787@gmail.com>
2024-10-25 09:05:05 -0400

    homeAppbarSubTitle now displays correct day of week

--- lib/features/app/screens/home/widgets/home_appbar.dart
+++ lib/features/app/screens/home/widgets/home_appbar.dart
@@ -8,6 +8,7 @@ import 'package:grades/utils/constants/text_strings.dart';
 import 'package:grades/utils/device/device_utilities.dart';
 import 'package:grades/utils/helpers/helper_functions.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../../utils/local_storage/storage_utility.dart';
 import '../../../../authentication/screens/login/widgets/login_policy_view.dart';
@@ -28,7 +29,7 @@ class GenesisHomeAppBar extends StatelessWidget {
           Text(GenesisTexts.homeAppbarTitle + user.getUsername(),
               style: Theme.of(context).textTheme.headlineLarge!.apply(
                   color: dark ? GenesisColors.grey : GenesisColors.black)),
-          Text(GenesisTexts.homeAppbarSubTitle,
+          Text("${GenesisTexts.homeAppbarSubTitle} ${DateFormat('EEEE').format(DateTime.now())}.",
               style: Theme.of(context)
                   .textTheme
                   .bodyMedium!
